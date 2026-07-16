0e6df82dd855b1ed14e6c4ee00bb1f9841eef5bb
Gabriel Ramos <whosramoss@gmail.com>
2026-07-16 02:04:19 -0300

    build: add vite and react package toolchain

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'node:path'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, 'packages/react/src'),
+        },
+      ],
+    },
+  }
+})
