8c0e998a35af78d1ca40924ccca493d11750d510
Gabriel Ramos <whosramoss@gmail.com>
2026-07-16 02:04:24 -0300

    feat: add avatar and spinner components

---
+++ packages/react/__stories__/components/avatar.stories.tsx
@@ -0,0 +1,31 @@
+import type { StoryObj } from '@storybook/react'
+
+import { AvatarFallback, AvatarImage, Avatar } from '@/components/avatar'
+
+const meta = {
+  title: 'components/Avatar',
+  component: Avatar,
+  argTypes: {
+    hasBorder: {
+      control: 'boolean',
+    },
+  },
+  args: {
+    children: (
+      <Avatar>
+        <AvatarImage
+          src="https://github.com/whosramoss.png"
+          alt="Gustavo Rodrigues"
+        />
+        <AvatarFallback>GR</AvatarFallback>
+      </Avatar>
+    ),
+  },
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export default meta
+
+export const Basic: StoryObj = {}
